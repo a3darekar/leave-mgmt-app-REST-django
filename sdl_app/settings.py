f2d9c76034c44cd71387284e92c19ffb8a990eb1
knowhere1998 <darekar.amey@gmail.com>
2018-08-21 21:13:31 +0530

    Added Static root

--- sdl_app/settings.py
+++ sdl_app/settings.py
@@ -142,12 +142,14 @@ USE_L10N = True
 
 USE_TZ = True
 
-LOGIN_URL = 'login'
-LOGIN_REDIRECT_URL = 'home'
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = '/static/'
 
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
